1de9d734b788e36c85e825792486ef4705377208
Samuel Peet <samuelpeet@gmail.com>
2018-07-08 16:40:40 +1000

    Travis integration 5

--- tests/test_conehead.py
+++ tests/test_conehead.py
@@ -5,7 +5,7 @@ from conehead.geometry import beam_to_global, global_to_beam
 
 class TestSourceMovements:
     def test_gantry_0(self):
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(0)
         correct_pos = np.array([0, 0, 100])
         np.testing.assert_array_almost_equal(
@@ -17,7 +17,7 @@ class TestSourceMovements:
         )
 
     def test_gantry_45(self):
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(45)
         correct_pos = np.array([np.cos(np.pi/4)*100, 0, np.sin(np.pi/4)*100])
         np.testing.assert_array_almost_equal(
@@ -29,7 +29,7 @@ class TestSourceMovements:
         )
 
     def test_gantry_225(self):
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(225)
         correct_pos = np.array(
             [-np.cos(np.pi/4)*100, 0, -np.cos(np.pi/4)*100]
@@ -43,7 +43,7 @@ class TestSourceMovements:
         )
 
     def test_gantry_270(self):
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(270)
         correct_pos = np.array([-100, 0, 0])
         np.testing.assert_array_almost_equal(
@@ -55,7 +55,7 @@ class TestSourceMovements:
         )
 
     def test_collimator_90(self):
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.collimator(90)
         correct_rot = np.array([0, 0, np.pi/2])
         np.testing.assert_array_almost_equal(
@@ -63,7 +63,7 @@ class TestSourceMovements:
         )
 
     def test_collimator_270(self):
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.collimator(270)
         correct_rot = np.array([0, 0, np.pi*3/2])
         np.testing.assert_array_almost_equal(
@@ -74,7 +74,7 @@ class TestSourceMovements:
 class TestCoordinateTranformations:
     def test_beam_to_global_G0_C0(self):
         """ Test at G0, C0 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(0)
         source.collimator(0)
         beam_coords = np.array([.1, .2, .3])
@@ -86,7 +86,7 @@ class TestCoordinateTranformations:
 
     def test_beam_to_global_G90_C0(self):
         """ Test at G90, C0 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(90)
         source.collimator(0)
         beam_coords = np.array([.1, .2, .3])
@@ -98,7 +98,7 @@ class TestCoordinateTranformations:
 
     def test_beam_to_global_G270_C0(self):
         """ Test at G270, C0 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(270)
         source.collimator(0)
         beam_coords = np.array([.1, .2, .3])
@@ -110,7 +110,7 @@ class TestCoordinateTranformations:
 
     def test_beam_to_global_G0_C90(self):
         """ Test at G0, C90 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(0)
         source.collimator(90)
         beam_coords = np.array([.1, .2, .3])
@@ -122,7 +122,7 @@ class TestCoordinateTranformations:
 
     def test_beam_to_global_G270_C270(self):
         """ Test at G270, C270 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(270)
         source.collimator(270)
         beam_coords = np.array([.1, .2, .3])
@@ -134,7 +134,7 @@ class TestCoordinateTranformations:
 
     def test_global_to_beam_G0_C0(self):
         """ Test at G0, C0 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(0)
         source.collimator(0)
         global_coords = np.array([.1, .2, .3])
@@ -146,7 +146,7 @@ class TestCoordinateTranformations:
 
     def test_global_to_beam_G90_C0(self):
         """ Test at G90, C0 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(90)
         source.collimator(0)
         global_coords = np.array([.1, .2, .3])
@@ -158,7 +158,7 @@ class TestCoordinateTranformations:
 
     def test_global_to_beam_G270_C0(self):
         """ Test at G270, C0 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(270)
         source.collimator(0)
         global_coords = np.array([.1, .2, .3])
@@ -170,7 +170,7 @@ class TestCoordinateTranformations:
 
     def test_global_to_beam_G0_C90(self):
         """ Test at G0, C90 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(0)
         source.collimator(90)
         global_coords = np.array([.1, .2, .3])
@@ -182,7 +182,7 @@ class TestCoordinateTranformations:
 
     def test_global_to_beam_G270_C270(self):
         """ Test at G270, C270 """
-        source = Source()
+        source = Source("varian_clinac_6MV")
         source.gantry(270)
         source.collimator(270)
         global_coords = np.array([.1, .2, .3])
