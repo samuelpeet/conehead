49630c3fc353e2529c785b0eab2f1fd7b6851317
Samuel Peet <samuel.peet@gmail.com>
2018-10-19 17:33:06 +1000

    First implementation of horn factor

--- example.py
+++ example.py
@@ -10,21 +10,24 @@ plan = pydicom.dcmread("RP.3DCRT.dcm", force=True)
 
 # Choose source
 source = Source("varian_clinac_6MV")
-source.gantry(15)
-source.collimator(45)
+source.gantry(0)
+source.collimator(0)
 
 # Set the jaws and MLC
-block = Block(source.rotation, plan=plan)
+# block = Block(source.rotation, plan=plan)
+block = Block()
+block.set_square(30)
 
 # Use a simple cubic phantom
 phantom = SimplePhantom()
 
 # Calculation settings
 settings = {
-    'stepSize': 0.1,  # Stepsize when raytracing effective depth
-    'sPri': 1.0,  # Primary source strength (photons/mm^2)
-    'softRatio': 0.0025,  # mm^-1
+    'stepSize': 0.1,  # Stepsize when raytracing effective depth (cm)
+    'sPri': 1.0,  # Primary source strength (photons/cm^2)
+    'softRatio': 0.0025,  # cm^-1
     'softLimit': 20,  # cm
+    'hornRatio': 0.006,  # % per cm
     'eLow': 0.01,  # MeV
     'eHigh': 7.0,  # MeV
     'eNum': 500,  # Spectrum samples
