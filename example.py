050561aee43683a6acb7ab2a825aa909929847f8
Sam Peet <samuelpeet@gmail.com>
2025-10-08 16:49:34 +1000

    Add TOML configuration for source and energy spectrum settings

--- example.py
+++ example.py
@@ -6,6 +6,7 @@ from conehead.conehead import Conehead
 import numpy as np
 import pydicom
 from scipy.interpolate import RegularGridInterpolator
+import toml
 
 # Load test plan
 plan = pydicom.dcmread("RP.3DCRT.dcm", force=True)
@@ -23,39 +24,12 @@ block.set_square(10)
 # Use a simple cubic phantom
 phantom = SimplePhantom()
 
-# Calculation settings
-settings = {
-    # 'stepSize': 0.1,  # Stepsize when raytracing effective depth (cm)
-    'sPri': 0.90924,  # Primary source strength
-    'sAnn': 2.887e-3,  # Annular source strength
-    'zAnn': 4.0,  # Distance of annular source from point source (cm)
-    'rInner': 0.2,  # Inner radius of annular source
-    'rOuter': 1.4,  # Outer radius of annular source
-    'zExp': 12.5,  # Distance of exponential source from point source (cm)
-    'sExp': 8.289e-3,  # Exponential source strength
-    'kExp': 0.4816,  # Exponential source exponent coefficient
-    'softRatio': 0.0025,  # cm^-1
-    'softLimit': 20,  # cm
-    'hornRatio': 0.0065,  # % per cm
-    # 'eLow': 0.01,  # MeV
-    # 'eHigh': 7.0,  # MeV
-    # 'eNum': 500,  # Spectrum samples
-    'fluenceResampling': 3,  # Split voxels for fluence calculation
-    'energy_weights': {  # Varian Clinac iX 6MV
-        "0.5": 0.08196,
-        "1.0": 0.12385,
-        "1.5": 0.10605,
-        "2.0": 0.08307,
-        "2.5": 0.05881,
-        "3.0": 0.03911,
-        "3.5": 0.02131,
-        "4.0": 0.02426,
-        "4.5": 0.0,
-        "5.0": 0.00881,
-        "5.5": 0.0,
-        "6.0": 0.00498,
-    }    
-}
+
+# Load calculation settings from TOML file
+settings_toml = toml.load("settings.toml")
+settings = settings_toml["calculation"]
+settings["energy_weights"] = settings_toml["energy_weights"]
+
 
 conehead = Conehead()
 conehead.calculate(source, block, phantom, settings)
