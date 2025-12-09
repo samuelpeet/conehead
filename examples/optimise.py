617d91ac4c51495f2a41285e05adb274e5418313
Sam Peet <samuelpeet@gmail.com>
2025-12-10 09:54:55 +1000

    Fixed BPC optimisation bug

--- examples/optimise.py
+++ examples/optimise.py
@@ -1,6 +1,7 @@
 # %%
 import sys
 import os
+
 sys.path.append(os.path.join(os.getcwd(), "../build/"))
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,31 +27,64 @@ def import_gold_beam_data():
         gold[v] = {}
         gold[v]["of"] = ofs[i]
         df = pd.read_excel("6FFF Beam Data.xlsx", sheet_name="Open Field Depth Dose")
-        gold[v]["pdd"] = [df.iloc[5:, 0].to_numpy().astype(np.float32), df.iloc[5:, i + 1].to_numpy().astype(np.float32) / 100]
+        gold[v]["pdd"] = [
+            df.iloc[5:, 0].to_numpy().astype(np.float32),
+            df.iloc[5:, i + 1].to_numpy().astype(np.float32) / 100,
+        ]
         df = pd.read_excel("6FFF Beam Data.xlsx", sheet_name="Open Field Profiles at 1.5cm")
-        gold[v]["prof_15"] = [df.iloc[7:, 0].to_numpy().astype(np.float32), df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][15]]
+        gold[v]["prof_15"] = [
+            df.iloc[7:, 0].to_numpy().astype(np.float32),
+            df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][15],
+        ]
         df = pd.read_excel("6FFF Beam Data.xlsx", sheet_name="Open Field Profiles at 5cm")
-        gold[v]["prof_50"] = [df.iloc[7:, 0].to_numpy().astype(np.float32), df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][50]]    
+        gold[v]["prof_50"] = [
+            df.iloc[7:, 0].to_numpy().astype(np.float32),
+            df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][50],
+        ]
         df = pd.read_excel("6FFF Beam Data.xlsx", sheet_name="Open Field Profiles at 10cm")
-        gold[v]["prof_100"] = [df.iloc[7:, 0].to_numpy().astype(np.float32), df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][100]]
+        gold[v]["prof_100"] = [
+            df.iloc[7:, 0].to_numpy().astype(np.float32),
+            df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][100],
+        ]
         df = pd.read_excel("6FFF Beam Data.xlsx", sheet_name="Open Field Profiles at 20cm")
-        gold[v]["prof_200"] = [df.iloc[7:, 0].to_numpy().astype(np.float32), df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][200]]
+        gold[v]["prof_200"] = [
+            df.iloc[7:, 0].to_numpy().astype(np.float32),
+            df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][200],
+        ]
         df = pd.read_excel("6FFF Beam Data.xlsx", sheet_name="Open Field Profiles at 30cm")
-        gold[v]["prof_300"] = [df.iloc[7:, 0].to_numpy().astype(np.float32), df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][300]]
+        gold[v]["prof_300"] = [
+            df.iloc[7:, 0].to_numpy().astype(np.float32),
+            df.iloc[7:, i + 1].to_numpy().astype(np.float32) / 100 * gold[v]["pdd"][1][300],
+        ]
     df = pd.read_excel("6FFF Beam Data.xlsx", sheet_name="Diagonal Profiles")
-    gold[40]["diag_15"] = [df.iloc[6:, 0].to_numpy().astype(np.float32), df.iloc[6:, 1].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][15]]
+    gold[40]["diag_15"] = [
+        df.iloc[6:, 0].to_numpy().astype(np.float32),
+        df.iloc[6:, 1].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][15],
+    ]
     gold[40]["diag_15"][0] = gold[40]["diag_15"][0][~np.isnan(gold[40]["diag_15"][1])]
     gold[40]["diag_15"][1] = gold[40]["diag_15"][1][~np.isnan(gold[40]["diag_15"][1])]
-    gold[40]["diag_50"] = [df.iloc[6:, 0].to_numpy().astype(np.float32), df.iloc[6:, 2].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][50]]
+    gold[40]["diag_50"] = [
+        df.iloc[6:, 0].to_numpy().astype(np.float32),
+        df.iloc[6:, 2].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][50],
+    ]
     gold[40]["diag_50"][0] = gold[40]["diag_50"][0][~np.isnan(gold[40]["diag_50"][1])]
     gold[40]["diag_50"][1] = gold[40]["diag_50"][1][~np.isnan(gold[40]["diag_50"][1])]
-    gold[40]["diag_100"] = [df.iloc[6:, 0].to_numpy().astype(np.float32), df.iloc[6:, 3].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][100]]
+    gold[40]["diag_100"] = [
+        df.iloc[6:, 0].to_numpy().astype(np.float32),
+        df.iloc[6:, 3].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][100],
+    ]
     gold[40]["diag_100"][0] = gold[40]["diag_100"][0][~np.isnan(gold[40]["diag_100"][1])]
     gold[40]["diag_100"][1] = gold[40]["diag_100"][1][~np.isnan(gold[40]["diag_100"][1])]
-    gold[40]["diag_200"] = [df.iloc[6:, 0].to_numpy().astype(np.float32), df.iloc[6:, 4].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][200]]
+    gold[40]["diag_200"] = [
+        df.iloc[6:, 0].to_numpy().astype(np.float32),
+        df.iloc[6:, 4].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][200],
+    ]
     gold[40]["diag_200"][0] = gold[40]["diag_200"][0][~np.isnan(gold[40]["diag_200"][1])]
     gold[40]["diag_200"][1] = gold[40]["diag_200"][1][~np.isnan(gold[40]["diag_200"][1])]
-    gold[40]["diag_300"] = [df.iloc[6:, 0].to_numpy().astype(np.float32), df.iloc[6:, 5].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][300]]
+    gold[40]["diag_300"] = [
+        df.iloc[6:, 0].to_numpy().astype(np.float32),
+        df.iloc[6:, 5].to_numpy().astype(np.float32) / 100 * gold[40]["pdd"][1][300],
+    ]
     gold[40]["diag_300"][0] = gold[40]["diag_300"][0][~np.isnan(gold[40]["diag_300"][1])]
     gold[40]["diag_300"][1] = gold[40]["diag_300"][1][~np.isnan(gold[40]["diag_300"][1])]
     return gold
@@ -88,56 +122,67 @@ def calculate_doses(fss, exam, settings):
     for fs in fss:
         print(f"Running field: {fs}")
         calc[fs] = {}
-        calc[fs]["dose"] = run(fs, grid, exam, settings) 
-        calc[fs]["dose"].values *= 100 # Multiplying by 100 for 100 MU (thus 1 Gy at dmax for 10x10)
-        ds = [grid.corner[1] + grid.resolution[1] / 2 + x * grid.resolution[1] for x in range(grid.num_voxels[1])]
-        calc[fs]["pdd"] = [ds, calc[fs]["dose"].values[grid.num_voxels[1] // 2, :, grid.num_voxels[1] // 2]]
-        xs = [grid.corner[0] + grid.resolution[0] / 2 + x * grid.resolution[0] for x in range(grid.num_voxels[0])]
+        calc[fs]["dose"] = run(fs, grid, exam, settings)
+        calc[fs][
+            "dose"
+        ].values *= 100  # Multiplying by 100 for 100 MU (thus 1 Gy at dmax for 10x10)
+        ds = [
+            grid.corner[1] + grid.resolution[1] / 2 + x * grid.resolution[1]
+            for x in range(grid.num_voxels[1])
+        ]
+        calc[fs]["pdd"] = [
+            ds,
+            calc[fs]["dose"].values[grid.num_voxels[1] // 2, :, grid.num_voxels[1] // 2],
+        ]
+        xs = [
+            grid.corner[0] + grid.resolution[0] / 2 + x * grid.resolution[0]
+            for x in range(grid.num_voxels[0])
+        ]
         calc[fs]["prof_15"] = [xs, calc[fs]["dose"].values[grid.num_voxels[2] // 2, 7, :]]
         calc[fs]["prof_50"] = [
             xs,
-            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 24, :] + 
-            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 25, :]
+            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 24, :]
+            + 0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 25, :],
         ]
         calc[fs]["prof_100"] = [
             xs,
-            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 49, :] + 
-            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 50, :]
+            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 49, :]
+            + 0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 50, :],
         ]
         calc[fs]["prof_200"] = [
             xs,
-            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 99, :] + 
-            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 100, :]
+            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 99, :]
+            + 0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 100, :],
         ]
         calc[fs]["prof_300"] = [
             xs,
-            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 149, :] + 
-            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 150, :]
+            0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 149, :]
+            + 0.5 * calc[fs]["dose"].values[grid.num_voxels[2] // 2, 150, :],
         ]
         if fs == 40:
             xs = np.array(xs)
             rs = np.sqrt(xs * xs + xs * xs)
-            rs[xs < 1] *= -1
+            rs[xs < 0] *= -1
             calc[fs]["diag_15"] = [rs, np.diag(calc[fs]["dose"].values[:, 7, :])]
             calc[fs]["diag_50"] = [
                 rs,
-                0.5 * np.diag(calc[fs]["dose"].values[:, 24, :]) +
-                0.5 * np.diag(calc[fs]["dose"].values[:, 25, :])
+                0.5 * np.diag(calc[fs]["dose"].values[:, 24, :])
+                + 0.5 * np.diag(calc[fs]["dose"].values[:, 25, :]),
             ]
             calc[fs]["diag_100"] = [
                 rs,
-                0.5 * np.diag(calc[fs]["dose"].values[:, 49, :]) +
-                0.5 * np.diag(calc[fs]["dose"].values[:, 50, :])
+                0.5 * np.diag(calc[fs]["dose"].values[:, 49, :])
+                + 0.5 * np.diag(calc[fs]["dose"].values[:, 50, :]),
             ]
             calc[fs]["diag_200"] = [
                 rs,
-                0.5 * np.diag(calc[fs]["dose"].values[:, 99, :]) +
-                0.5 * np.diag(calc[fs]["dose"].values[:, 100, :])
+                0.5 * np.diag(calc[fs]["dose"].values[:, 99, :])
+                + 0.5 * np.diag(calc[fs]["dose"].values[:, 100, :]),
             ]
             calc[fs]["diag_300"] = [
                 rs,
-                0.5 * np.diag(calc[fs]["dose"].values[:, 149, :]) +
-                0.5 * np.diag(calc[fs]["dose"].values[:, 150, :])
+                0.5 * np.diag(calc[fs]["dose"].values[:, 149, :])
+                + 0.5 * np.diag(calc[fs]["dose"].values[:, 150, :]),
             ]
     return calc
 
@@ -145,14 +190,16 @@ def calculate_doses(fss, exam, settings):
 def difference_in_pdds(calc, gold):
     diff = 0.0
     for fs in calc.keys():
-        calc_pdd_interp = np.interp(gold[fs]["pdd"][0], calc[fs]["pdd"][0], calc[fs]["pdd"][1] / calc[fs]["pdd"][1].max())
+        calc_pdd_interp = np.interp(
+            gold[fs]["pdd"][0], calc[fs]["pdd"][0], calc[fs]["pdd"][1] / calc[fs]["pdd"][1].max()
+        )
         gold_pdd = gold[fs]["pdd"][1]
 
         # Clip off approximate build-up region
         calc_pdd_interp = calc_pdd_interp[10:]
         gold_pdd = gold_pdd[10:]
 
-        diff += np.sum(np.abs(calc_pdd_interp - gold_pdd)**2)
+        diff += np.sum(np.abs(calc_pdd_interp - gold_pdd) ** 2)
     return diff
 
 
@@ -160,7 +207,9 @@ def optimise_pdd(x, exam, gold):
     # Update settings with new energy spectrum
     settings = toml.load("Truebeam_6FFF_M120.toml")
     energies = np.array(settings["energy_spectrum"]["energies"], dtype=np.float32)
-    energy_weights = np.array([x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], 0.0, x[8], 0.0, x[9]], dtype=np.float32)
+    energy_weights = np.array(
+        [x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], 0.0, x[8], 0.0, x[9]], dtype=np.float32
+    )
     N = np.sum(energy_weights)
     energy_weights /= N
     energy_weights /= energies
@@ -178,19 +227,52 @@ def optimise_pdd(x, exam, gold):
 # Optimising energy spectrum
 gold = import_gold_beam_data()
 exam = Exam(dicom_dir="40", hu_lut_path="Siemens_Confidence.toml")
-x0 = [0.13077393, 0.08785954, 0.0294463, 0.04815249, 0.03111974, 0.43969824, 0.03215145, 0.0169554, 0.03014422, 0.022631]
-bounds = [(0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0),
-          (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0),
-          (0.0, 1.0), (0.0, 1.0)]
+x0 = [
+    0.13077393,
+    0.08785954,
+    0.0294463,
+    0.04815249,
+    0.03111974,
+    0.43969824,
+    0.03215145,
+    0.0169554,
+    0.03014422,
+    0.022631,
+]
+bounds = [
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+]
 result = minimize(optimise_pdd, x0, args=(exam, gold), method="Nelder-Mead", bounds=bounds)
 
 # %%
 # Plot PDDs
 # x = [0.11815628, 0.07121678, 0.14556541, 0.09402617, 0.10339564, 0.09395615, 0.07655023, 0.11007001, 0.0512248, 0.0117276]
-x = [0.13077393, 0.08785954, 0.0294463, 0.04815249, 0.03111974, 0.43969824, 0.03215145, 0.0169554, 0.03014422, 0.022631]
+x = [
+    0.13077393,
+    0.08785954,
+    0.0294463,
+    0.04815249,
+    0.03111974,
+    0.43969824,
+    0.03215145,
+    0.0169554,
+    0.03014422,
+    0.022631,
+]
 settings = toml.load("Truebeam_6FFF_M120.toml")
 energies = np.array(settings["energy_spectrum"]["energies"], dtype=np.float32)
-energy_weights = np.array([x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], 0.0, x[8], 0.0, x[9]], dtype=np.float32)
+energy_weights = np.array(
+    [x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], 0.0, x[8], 0.0, x[9]], dtype=np.float32
+)
 N = np.sum(energy_weights)
 energy_weights /= N
 energy_weights /= energies
@@ -200,10 +282,13 @@ calc = calculate_doses(fss, exam, settings)
 fig, ax = plt.subplots(1, 1, figsize=(16, 9))
 for fs in fss:
     ax.plot(gold[fs]["pdd"][0], gold[fs]["pdd"][1], label="Measured PDD")
-    ax.plot(calc[fs]["pdd"][0], calc[fs]["pdd"][1] / calc[fs]["pdd"][1].max(), '.', label="Conehead PDD")
+    ax.plot(
+        calc[fs]["pdd"][0], calc[fs]["pdd"][1] / calc[fs]["pdd"][1].max(), ".", label="Conehead PDD"
+    )
     ax.set_xlabel("Depth (cm)")
     ax.set_ylabel("Relative Dose")
     ax.legend()
+
 
 # %%
 def calculate_normalisation():
@@ -215,6 +300,8 @@ def calculate_normalisation():
     calc_dose_at_10cm = calc[10]["pdd"][1][d_max]
     normalisation = 1 / calc_dose_at_10cm
     return normalisation
+
+
 print(calculate_normalisation())
 
 
@@ -231,13 +318,14 @@ def calculate_ofcs(fss, exam, settings, gold):
         calc[fs]["ofc"] = ofc
     return calc
 
+
 gold = import_gold_beam_data()
 exam = Exam(dicom_dir="40", hu_lut_path="Siemens_Confidence.toml")
 settings = toml.load("Truebeam_6FFF_M120.toml")
 fss = [3, 4, 6, 8, 10, 20, 30, 40]
 calc = calculate_ofcs(fss, exam, settings, gold)
 for fs in calc.keys():
-    calc[fs]["ofc"] /= calc[10]["ofc"] # Normalise to 10 x 10
+    calc[fs]["ofc"] /= calc[10]["ofc"]  # Normalise to 10 x 10
 print("Output Factor Corrections:")
 print([calc[fs]["ofc"] for fs in fss])
 
@@ -247,15 +335,18 @@ def difference_in_diag(calc, gold):
     diff = 0.0
     fs = 40
     key = "diag_15"
-    calc_diag_interp = np.interp(gold[fs][key][0], calc[fs][key][0], calc[fs][key][1] / calc[fs][key][1].max())
-    gold_diag = gold[fs][key][1]
+    calc_diag_interp = np.interp(
+        gold[fs][key][0], calc[fs][key][0], calc[fs][key][1] / calc[fs][key][1].max()
+    )
+    gold_diag = gold[fs][key][1] / gold[fs][key][1].max()
 
     # Clip off tails
     calc_diag_interp = calc_diag_interp[50:-50]
     gold_diag = gold_diag[50:-50]
 
-    diff += np.sum(np.abs(calc_diag_interp - gold_diag)**2)
+    diff += np.sum(np.abs(calc_diag_interp - gold_diag) ** 2)
     return diff
+
 
 def optimise_bpc(x, exam, gold):
     # Update settings with new beam profile correction
@@ -267,13 +358,58 @@ def optimise_bpc(x, exam, gold):
     print(f"Diff: {diff}")
     return diff
 
+
 # Optimising beam profile correction
 gold = import_gold_beam_data()
 exam = Exam(dicom_dir="40", hu_lut_path="Siemens_Confidence.toml")
-x0 = [0.996, 0.982, 0.961, 0.936, 0.881, 0.822, 0.765, 0.720, 0.677, 0.631, 0.596, 0.562, 0.533, 0.51, 0.5, 0.388, 0.069, 0.043, 0.03, 0.02, 0.02, 0.02]
-bounds = [(0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0),
-          (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0), (0.0, 1.0),
-          (0.0, 1.0), (0.0, 1.0)]
+x0 = [
+    0.996,
+    0.982,
+    0.961,
+    0.936,
+    0.881,
+    0.822,
+    0.765,
+    0.720,
+    0.677,
+    0.631,
+    0.596,
+    0.562,
+    0.533,
+    0.51,
+    0.5,
+    0.388,
+    0.069,
+    0.043,
+    0.03,
+    0.02,
+    0.02,
+    0.02,
+]
+bounds = [
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+    (0.0, 1.0),
+]
 result = minimize(optimise_bpc, x0, args=(exam, gold), method="Nelder-Mead", bounds=bounds)
 
 
